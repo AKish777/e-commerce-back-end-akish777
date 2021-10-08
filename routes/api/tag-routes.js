b4767db0501fba508334ea6ec899a58c8297a53e
Alex Kichigin <alexkichigin@Alexs-MacBook-Pro.local>
2021-10-08 16:46:49 -0400

    added code for tagroute

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -53,8 +53,36 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+router.put('/:id', async (req, res) => {
+  try {
+    const tag = await Tag.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    const productTags = await ProductTag.findAll({ where: { tag_id: req.params.id } });
+    const productTagIds = productTags.map(({ product_id }) => product_id);
+    const newProductTags = req.body.productIds
+      .filter((product_id) => !productTagIds.includes(product_id))
+      .map((product_id) => {
+        return {
+          tag_id: req.params.id,
+          product_id,
+        };
+      });
+    const productTagsToRemove = productTags
+    .filter(({ product_id }) => !req.body.productIds.includes(product_id))
+    .map(({ id }) => id);
+
+    const updatedProductTags = await Promise.all([
+      ProductTag.destroy({ where: { id: productTagsToRemove } }),
+      ProductTag.bulkCreate(newProductTags),
+    ]);
+    res.status(200).json(updatedProductTags);
+  }
+  catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
